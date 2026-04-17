fb56563eef653a5b09b3304c19b6a2cc9e435086
Lelle <wilhelmsson9911@gmail.com>
2026-04-17 14:49:30 +0200

    Test biome

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import MainLayout from './components/layout/MainLayout';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
